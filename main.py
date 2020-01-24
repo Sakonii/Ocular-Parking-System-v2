0ce6023eed8db64581d245b978b47520d440e377
Utsav Maskey <um02409118@student.ku.edu.np>
2020-01-24 20:21:32 +0545

    Detection-inference-implemented

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import argparse
 
-from segmentation import Segmentation, acc_camvid
+from detection import Detection
 #from inference import ImageObjects
 #from inpainting import FeatureLoss
 
 
 def main():
 
-    #img, mask, classes = Segmentation(
-    #    fname_img=args.image, model=args.model_segmentation
-    #).start_segmentation()
+    Detection(
+       fname_img=args.image, model_pth=args.model_detection
+    ).start_detection()
     # ImageObjects(img, mask, classes, model=args.model_inpainting).inference()
-
+    return
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Image File Name")
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         "--image", type=str, default="img.png", help="Enter the image file name"
     )
     parser.add_argument(
-        "--model_segmentation",
+        "--model_detection",
         type=str,
         default="detection_retina_256.pth",
         help="Pre-trained Weights for RetinNet Detection",
